0d490c8be2b23f21cb8c401381e3894464c7912b
ranujap00 <ranujap00@gmail.com>
2023-09-19 17:36:10 +0530

    best way to handle missing values

--- fineTuning/missing_val_impact.py
+++ fineTuning/missing_val_impact.py
@@ -1,20 +1,25 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
+import pandas as pd
+from sklearn.impute import SimpleImputer
 
 df = pd.read_csv('Water Quality Prediction.csv')
 df = df.sample(n=100000, random_state=42)
 
 # remove all rows with Nan values
-
-df.dropna(inplace=True) # modifies the dataframe in place
+# df.dropna(inplace=True) # modifies the dataframe in place
 
 encoded_data = pd.get_dummies(df, columns=['Month', 'Color', 'Source'])
 
 X = encoded_data.drop(['Potability', 'Index'], axis=1) # axis=1 indicates we are dropping a column, not a row
 Y = encoded_data['Potability']
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='median')
+imputer.fit(X)
+X = imputer.transform(X)  # returns the updated columns
 
 # Assuming X contains your feature columns and y contains 'Potability'
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
@@ -29,22 +34,11 @@ y_pred = rfc_classifier.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
 
-# Get feature importances
-feature_importances = rfc_classifier.feature_importances_
 
-features = []
-importances = []
-# Print or visualize feature importances
-for feature_name, importance in zip(X.columns, feature_importances):
-    print(f"{feature_name}: {importance}")
-    features.append(feature_name)
-    importances.append(importance)
+### accuracy 
 
-
-# Create a bar chart
-plt.figure(figsize=(12, 6))
-plt.barh(features, importances)
-plt.xlabel('Feature Importance')
-plt.title('Feature Importances')
-plt.gca().invert_yaxis()
-plt.show()
+# remove missing values - 0.8618007777445408
+# replace with the mean - 0.86755
+# replace with the median - 0.8681 (BEST ONE)
+# replace with the most_frequent - 0.8662
+# replace with the constant - 0.86605
